87f8fd58809cca62c44b1ed1520e7c9afeddfd04
Somuch-make <1993700090@qq.com>
2020-06-21 18:49:25 +0800

    王静杰上传了首页

---
+++ 1.php
@@ -0,0 +1,91 @@
+<?php
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<title></title>
+		<style type="text/css">
+			h1{
+				color:#8694AF ;
+			}
+			#Nav{
+				padding:20px; font-size: 18px; font-weight: 900;
+			}
+			.select{
+				font-weight: 900; 
+			}
+			#div1{
+				animation: 12s wacthtv infinite;
+				width: 2000px;
+				height: 1000px;
+				
+			}
+			#div1 img{
+				float:left;
+				}
+			#tv{
+					align-content: center;
+					width: 500px;
+					height: 200px;
+					overflow: hidden;
+					margin-left: 100px;
+				}
+			@keyframes wacthtv{
+					0%{}
+					25%{transform: translateX(0px);}
+					30%{transform: translateX(-500px);}
+					50%{transform: translateX(-500px);}
+					55%{transform: translateX(-1000px);}
+					75%{transform: translateX(-1000px);}
+					80%{transform: translateX(-1500px);}
+					100%{transform: translateX(-1500px);}
+				}
+		</style>
+		
+	</head>
+	<body>
+		<table border="0" cellspacing="" cellpadding="" align="center" width="700" height="500">
+			<tr>
+				<td align="center"><img src="img/pic/7.jpg"  width="250" height="200"/></td>
+				<td width="400"><p align="center"><font color="#8694AF" size="7" face="楷体" ><b> 网上书店</b></font></p>
+				</td>
+			</tr>
+			<tr>
+				<td colspan="2" height="100">
+					<div align="center"  >
+						<span id="Nav">首页</span>
+						<span id="Nav">所有图书</span>
+						<span id="Nav">所有图书（分页）</span>
+						<span id="Nav">插入书籍</span>
+						<span id="Nav">编辑删除图书</span>
+					</div>
+				</td>
+			</tr>
+			<tr>
+				<td colspan="2">
+					<div id="tv">
+						<div id="div1" >
+							<img src="img/pic/3.jpg" width="500" height="200"  >
+							<img src="img/pic/4.jpg" width="500" height="200">
+							<img src="img/pic/5.jpg" width="500" height="200">
+							<img src="img/pic/6.jpg" width="500" height="200" >
+						</div>
+					</div>
+				</td>
+			</tr>
+			<tr>
+				<td colspan="2"align="center" height="100"><form action="" method="post" class="select">请选择要查找的书籍类型：
+						<select name="books" id="">
+							<option value="">外语类籍</option>
+							<option value="">计算机类籍</option>
+							<option value="">医药类籍</option>
+							<option value="">娱乐类籍</option>
+							<option value="">军事类籍</option>
+					    </select>
+					    <input type="submit" value="提交" />
+				</form></td>
+			</tr>
+		</table>
+	</body>
+</html>
+?>
--- index.php
+++ index.php
@@ -1,73 +1,93 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-<title>网上书店</title>
-</head>
-<body bgcolor="#f4f4f4">
-	<table width="100%" border="0" align="center">
-		<tr>
-			<td width="27%" height="68" rowspan="2">
-				<img width="200" height="106" src="images/book_logo.jpg"/>
-			</td>
-			<td height="68" colspan="4">
-				<font face="隶书" size="+4" color="#cccc00">网上书店</font>
-			</td>
-			<td width="10%" rowspan="2">&nbsp;</td>
-		</tr>
-		<tr>
-			<td colspan="4" align="center">欢迎光临我们的网站</td>
-		</tr>
-		<tr>
-			<td width="15%" height="20" align="left" valign="middle">
-				<a href="index.php">首页</a>
-			</td>
-			<td width="15%" height="20" align="left" valign="middle">
-				<a href="querybook.php">查询图书</a>
-			</td>
-			<td width="20%" height="20"><a href="addbook.php">新增图书</a></td>
-			<td width="15%" height="20" align="left" valign="middle">
-				<a href="editbook.php">编辑图书信息</a>
-			</td>
-			<td width="20%" height="20" align="left" valign="middle">
-				<a href="deletebook.php">删除图书</a>
-			</td>
-			<td width="15%" height="20">&nbsp;</td>
-		</tr>
-		<tr>
-			<td height="169" colspan="6" align="center">
-			<table width="100%" border="0">
-				<tr>
-					<td colspan="4" align="center">书店所有图书</td>
-				</tr>
-				<tr valign="middle">
-					<td align="center">书名</td>
-					<td align="center">作者</td>
-					<td align="center">图书类型</td>
-					<td align="center">图书价格</td>
-				</tr>
-				<tr valign="middle" align="center">
-					<td>图书1</td>
-					<td>a</td>
-					<td>计算机类</td>
-					<td>20</td>
-				</tr>
-				<tr valign="middle" align="center">
-					<td>图书2</td>
-					<td>b</td>
-					<td>外语类</td>
-					<td>22</td>
-				</tr>
-				<tr valign="middle" align="center">
-					<td>图书3</td>
-					<td>c</td>
-					<td>医学类</td>
-					<td>23</td>
-				</tr>
-			</table>
-		</td>
-		</tr>
-	</table>
-</body>
+<?php require_once 'connection/conn.php';?>
+<?php
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<title></title>
+		<style type="text/css">
+			h1{
+				color:#8694AF ;
+			}
+			#Nav{
+				padding:20px; font-size: 18px; font-weight: 900;
+			}
+			.select{
+				font-weight: 900; 
+			}
+			#div1{
+				animation: 12s wacthtv infinite;
+				width: 2000px;
+				height: 1000px;
+				
+			}
+			#div1 img{
+				float:left;
+				}
+			#tv{
+					align-content: center;
+					width: 500px;
+					height: 200px;
+					overflow: hidden;
+					margin-left: 100px;
+				}
+			@keyframes wacthtv{
+					0%{}
+					25%{transform: translateX(0px);}
+					30%{transform: translateX(-500px);}
+					50%{transform: translateX(-500px);}
+					55%{transform: translateX(-1000px);}
+					75%{transform: translateX(-1000px);}
+					80%{transform: translateX(-1500px);}
+					100%{transform: translateX(-1500px);}
+				}
+		</style>
+		
+	</head>
+	<body>
+		<table border="0" cellspacing="" cellpadding="" align="center" width="700" height="500">
+			<tr>
+				<td align="center"><img src="img/pic/7.jpg"  width="250" height="200"/></td>
+				<td width="400"><p align="center"><font color="#8694AF" size="7" face="楷体" ><b> 网上书店</b></font></p>
+				</td>
+			</tr>
+			<tr>
+				<td colspan="2" height="100">
+					<div align="center"  >
+						<span id="Nav">首页</span>
+						<span id="Nav">所有图书</span>
+						<span id="Nav">所有图书（分页）</span>
+						<span id="Nav">插入书籍</span>
+						<span id="Nav">编辑删除图书</span>
+					</div>
+				</td>
+			</tr>
+			<tr>
+				<td colspan="2">
+					<div id="tv">
+						<div id="div1" >
+							<img src="img/pic/3.jpg" width="500" height="200"  >
+							<img src="img/pic/4.jpg" width="500" height="200">
+							<img src="img/pic/5.jpg" width="500" height="200">
+							<img src="img/pic/6.jpg" width="500" height="200" >
+						</div>
+					</div>
+				</td>
+			</tr>
+			<tr>
+				<td colspan="2"align="center" height="100"><form action="" method="post" class="select">请选择要查找的书籍类型：
+						<select name="books" id="">
+							<option value="">外语类籍</option>
+							<option value="">计算机类籍</option>
+							<option value="">医药类籍</option>
+							<option value="">娱乐类籍</option>
+							<option value="">军事类籍</option>
+					    </select>
+					    <input type="submit" value="提交" />
+				</form></td>
+			</tr>
+		</table>
+	</body>
 </html>
+?>
+<meta http-equiv="refresh" content="1>
