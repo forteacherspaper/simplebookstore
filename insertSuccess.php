5b0d4b6f7d15cc20dbdf4f8d0f191b0ec48f7dc0
ABC-0106 <3317559953@qq.com>
2020-06-21 16:11:23 +0800

    添加成功

---
+++ insertSuccess.php
@@ -0,0 +1,21 @@
+<?php require_once 'connection/conn.php';?>
+<?php
+    $Result1=false;  //假设插入不成功
+    if(isset($_POST['BookName'])){
+        $bookname=$_POST['BookName'];
+        $bookauthor=$_POST['BookAuthor'];
+        $bookprice=$_POST['BookPrice'];
+        $booktype=$_POST['BookType'];
+        $insertSQL="insert into booktable(bookname,bookauthor,booktype,bookprice) values('$bookname','$bookauthor',$booktype,$bookprice'";
+        $Result1= mysqli_query($conn, $insertSQL) or die(mysqli_errno($conn));
+    }
+    mysqli_close($conn);
+    if($Result1){
+        echo "<script>alter('插入成功！')；</script>";
+    }else{
+        echo "<script>alter('插入失败！')；</script>";
+    }
+?>
+<meta http-equiv="refresh" content="1； url=query.php">
+
+
