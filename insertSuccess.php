5ec0e5a3782c03480fc5115e419fa0551006ac69
ABC-0106 <3317559953@qq.com>
2020-06-25 20:41:05 +0800

    添加了插入书籍的处理页面

--- insertSuccess.php
+++ insertSuccess.php
@@ -6,14 +6,14 @@
         $bookauthor=$_POST['BookAuthor'];
         $bookprice=$_POST['BookPrice'];
         $booktype=$_POST['BookType'];
-        $insertSQL="insert into booktable(bookname,bookauthor,booktype,bookprice) values('$bookname','$bookauthor',$booktype,$bookprice'";
+        $insertSQL="insert into booktable(bookname,bookauthor,booktype,bookprice) values('$bookname','$bookauthor',$booktype,$bookprice)";
         $Result1= mysqli_query($conn, $insertSQL) or die(mysqli_error($conn));
     }
     mysqli_close($conn);
     if($Result1){
-        echo "<script>alter('插入成功！')；</script>";
+        echo "<script>alert('插入成功！');</script>";
     }else{
-        echo "<script>alter('插入失败！')；</script>";
+        echo "<script>alert('插入失败！');</script>";
     }
 ?>
 <meta http-equiv="refresh" content="1； url=allbooklist.php">
