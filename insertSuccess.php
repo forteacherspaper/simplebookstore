5d9265eb9430638d312e2cd90fbe14576d0b4f5d
ABC-0106 <3317559953@qq.com>
2020-06-26 13:52:04 +0800

    添添加处理页面

--- insertSuccess.php
+++ insertSuccess.php
@@ -1,4 +1,4 @@
-<?php require_once 'connection/conn.php';?>
+<?php require_once 'connections/conn.php';?>
 <?php
     $Result1=false;  //假设插入不成功
     if(isset($_POST['BookName'])){
@@ -6,16 +6,18 @@
         $bookauthor=$_POST['BookAuthor'];
         $bookprice=$_POST['BookPrice'];
         $booktype=$_POST['BookType'];
-        $insertSQL="insert into booktable(bookname,bookauthor,booktype,bookprice) values('$bookname','$bookauthor',$booktype,$bookprice'";
-        $Result1= mysqli_query($conn, $insertSQL) or die(mysqli_errno($conn));
+        $insertSQL="insert into booktable(bookname,bookauthor,booktype,bookprice) values('$bookname','$bookauthor',$booktype,$bookprice)";
+        $Result1= mysqli_query($conn, $insertSQL) or die(mysqli_error($conn));
     }
     mysqli_close($conn);
     if($Result1){
-        echo "<script>alter('插入成功！')；</script>";
+        echo "<script>alert('插入成功！');</script>";
     }else{
-        echo "<script>alter('插入失败！')；</script>";
+        echo "<script>alert('插入失败！');</script>";
     }
 ?>
-<meta http-equiv="refresh" content="1； url=querybook.php">
+<meta http-equiv="refresh" content="1;url=allbooklist.php">
+
+
 
 
