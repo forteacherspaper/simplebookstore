c78d8e2912243c086fe715485b6fd0e92152e5b7
sunlupeng2020 <slp2060@163.com>
2020-06-27 15:00:14 +0800

    修改了连接数据库的用户root的密码为root

--- connections/conn.php
+++ connections/conn.php
@@ -5,6 +5,6 @@
 $hostname_conn="localhost";
 $database_conn="bookdb";
 $username_conn="root";
-$password_conn="";
+$password_conn="root";
 $conn=mysqli_connect($hostname_conn,$username_conn,$password_conn,$database_conn) or trigger_error(mysqli_error(),E_USER_ERROR);//
 ?>
